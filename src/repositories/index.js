97788073af47759ec71745e5f12a5d48ce9f74ca
Haiver <haiverhercas@gmail.com>
2025-07-08 16:32:01 -0500

    correccion

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,6 +1,6 @@
 import Users from "../DAOs/factory.js"
 import UsersRepository from "./users.repositories.js"
 
-const userServices = new UsersRepository(new Users());
+const userServices = new UsersRepository(Users());
 
 export default userServices;
